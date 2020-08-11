5b6a664eb9f929f54c4d567293cc9c744b97baa6
Bryan Moffit <moffit@jlab.org>
2020-08-11 11:14:51 -0400

    Update compile line.
    Also update number of arguments for notify_notification_new.
    Compiles and runs fine.

--- ws-status-notify.c
+++ ws-status-notify.c
@@ -6,6 +6,12 @@
  * http://www.gnu.org/licenses/gpl.html
  */
 
+/*
+ * Updated compile line for CentOS 7
+ * cc -o ws-status-notify ws-status-notify.c `pkg-config libwnck-3.0 gtk+-3.0 libnotify --cflags --libs`
+ *
+ */
+
 /* To compile:
  *   cc -o ws-status-notify ws-status-notify.c `pkg-config libwnck-1.0 libnotify --cflags --libs`
  */
@@ -28,8 +34,7 @@ ws_status_workspace_changed(WnckScreen *wnck_screen,
     if(notif)
         notify_notification_update(notif, "Workspace Changed", name, "desktop");
     else {
-        notif = notify_notification_new("Workspace Changed", name,
-                                        "desktop", NULL);
+        notif = notify_notification_new("Workspace Changed", name, "desktop");
         g_signal_connect(G_OBJECT(notif), "closed",
                          G_CALLBACK(g_object_unref), NULL);
         g_object_add_weak_pointer(G_OBJECT(notif), (gpointer)&notif);
@@ -44,20 +49,20 @@ main(int argc,
      char **argv)
 {
     WnckScreen *wnck_screen;
-    
+
     gtk_init(&argc, &argv);
 
     if(!notify_init("Workspace Status")) {
         g_printerr("Failed to connect to notification daemon\n");
         return 1;
     }
-    
+
     wnck_screen = wnck_screen_get_default();
     wnck_screen_force_update(wnck_screen);
     g_signal_connect(G_OBJECT(wnck_screen), "active-workspace-changed",
                      G_CALLBACK(ws_status_workspace_changed), NULL);
-    
+
     gtk_main();
-    
+
     return 0;
 }
